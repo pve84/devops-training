b7209a8c931a1f049009da58aed70d186c7f9aad
Peter van Ekeren <peter.vanekeren@incentro.com>
2020-02-07 11:23:03 +0100

    Add tests

--- src/test/java/com/incentro/devops/api/ApiApplicationTests.java
+++ src/test/java/com/incentro/devops/api/ApiApplicationTests.java
@@ -1,13 +1,43 @@
 package com.incentro.devops.api;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class ApiApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApiApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApiApplicationTests {
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@LocalServerPort
+	private int port;
+
+	private String getRootUrl() {
+		return "http://localhost:" + port;
+	}
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+	}
+
+	@Test
+	public void testGetRandomInt() {
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/api/v1/int",
+				HttpMethod.GET, entity, String.class);
+
+		Assert.assertNotNull(response.getBody());
+		Integer.parseInt(response.getBody());
 	}
 
 }
